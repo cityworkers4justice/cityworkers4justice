4308313cc5d5cab36107512183073132f445f3f7
rlaake <robblaake@gmail.com>
2020-06-23 19:01:37 -0400

    add home page

--- scripts/navMenu.js
+++ scripts/navMenu.js
@@ -1,8 +1,10 @@
 let main = document.getElementsByTagName('main')[0];
+let navBar = document.getElementsByClassName('nav-bar')[0];
 let navDropdown = document.getElementsByClassName('nav-dropdown')[0];
 let navBarToggle = document.getElementsByClassName('fa-bars')[0];
 
 navBarToggle.addEventListener('click', function () {
+  navBar.classList.toggle('active-nav-bar');
   navDropdown.classList.toggle('active-nav-dropdown');
   main.classList.toggle('active-main');
   navBarToggle.classList.toggle('fa-bars')
@@ -11,8 +13,9 @@ navBarToggle.addEventListener('click', function () {
 
 function removeResponsiveClass() {
     if(window.innerWidth >= 768) {
-      main.classList.remove('active-main');
+      navBar.classList.remove('active-nav-bar');
       navDropdown.classList.remove('active-nav-dropdown');
+      main.classList.remove('active-main');
       navBarToggle.classList.remove('fa-times');
       navBarToggle.classList.add('fa-bars');
     }
